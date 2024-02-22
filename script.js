c7cf0b6f27165b774b2772bea97d3041f17d0efd
DaniAm94 <dani.amico94@gmail.com>
2024-02-22 16:11:48 +0100

    added api call into the script.js file to fetch data from index.php

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 console.log(Vue);
+const endpoint = 'http://localhost/boolean/php-todo-list-json/api/index.php';
 const { createApp } = Vue;
 
 const app = createApp({
     name: 'PHP ToDo List JSON',
     data: () => ({
-
-    })
+        tasks: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+        }).catch()
+    }
 })
 
 app.mount('#root');

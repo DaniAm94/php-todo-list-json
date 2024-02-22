2c25c0cc907d32308897239b61b7ebc329aabd9d
DaniAm94 <dani.amico94@gmail.com>
2024-02-22 18:11:16 +0100

    created add new task method, delete task method onworking

--- api/index.php
+++ api/index.php
@@ -2,6 +2,45 @@
 $dbs_path= __DIR__.'/../dbs/dbs.json';
 
 $dbs_content= file_get_contents($dbs_path);
+
+$tasks=$dbs_content;
+
+
+
+
+// Controllo se ho una task da cancellare
+$target_task = $_POST['target-task'] ?? '';
+// Se si
+if($target_task){
+    // Converto tasks in un array php
+    $tasks= json_decode($tasks, true);
+    // Elimino la task
+    
+    // foreach($tasks as $task){
+    //     if($task['id']===$target_task['id'])
+
+    // }
+    // Lo riconverto in json
+    $tasks=json_encode($new_tasks);
+    file_put_contents($dbs_path, $tasks);
+} 
+// Controllo se ho una nuova task
+$new_task = $_POST['new-task'] ?? '';
+// Se si
+if($new_task){
+    // Converto tasks in un array php
+    $tasks= json_decode($tasks, true);
+    // Aggiungo la nuova task
+    $tasks[] = $new_task;
+    // Lo riconverto in json
+    $tasks=json_encode($tasks);
+    file_put_contents($dbs_path, $tasks);
+} 
+
+
+
+
+
 header('Content-Type: application/json');
 
-echo $dbs_content;
+echo $tasks;
--- script.js
+++ script.js
@@ -5,8 +5,35 @@ const { createApp } = Vue;
 const app = createApp({
     name: 'PHP ToDo List JSON',
     data: () => ({
-        tasks: []
+        tasks: [],
+        newTaskText: ''
     }),
+    methods: {
+        addTask() {
+            const newTask = {
+                id: new Date().toISOString(),
+                text: this.newTaskText,
+                done: false
+            }
+            const data = { 'new-task': newTask };
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+                this.newTaskText = '';
+            })
+        },
+        deleteTask(taskId) {
+            const targetTask = this.tasks.find(task => task.id === taskId);
+            const data = { 'target-task': targetTask };
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+            })
+            console.log(targetTask);
+        }
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data;

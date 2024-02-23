391a623d75676286403698ca47f93022c68bff49
DaniAm94 <dani.amico94@gmail.com>
2024-02-24 00:26:52 +0100

    created add task api

--- api/index.php
+++ api/index.php
@@ -6,30 +6,18 @@ $dbs_content= file_get_contents($dbs_path);
 $tasks=$dbs_content;
 
 
-
-
-// Controllo se ho una task da cancellare
-$target_task = $_POST['target-task'] ?? '';
-// Se si
-if($target_task){
-    // Converto tasks in un array php
-    $tasks= json_decode($tasks, true);
-    // Elimino la task
-    
-    // foreach($tasks as $task){
-    //     if($task['id']===$target_task['id'])
-
-    // }
-    // Lo riconverto in json
-    $tasks=json_encode($new_tasks);
-    file_put_contents($dbs_path, $tasks);
-} 
 // Controllo se ho una nuova task
-$new_task = $_POST['new-task'] ?? '';
+$new_task_text = $_POST['new-task-text'] ?? '';
 // Se si
-if($new_task){
+if($new_task_text){
     // Converto tasks in un array php
     $tasks= json_decode($tasks, true);
+    // Creo la nuova task
+    $new_task=[
+        'id'=> uniqid(),
+        'text'=> $new_task_text,
+        'done'=> false
+    ];
     // Aggiungo la nuova task
     $tasks[] = $new_task;
     // Lo riconverto in json
